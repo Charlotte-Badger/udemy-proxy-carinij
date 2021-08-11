bab0edd2c8d78544645f3a28549f4c72fa9f6f34
wapu-lab <nieruoxi@hotmail.com>
2021-08-10 22:31:31 -0500

    Refactored the proxy server by updating the course content service part

--- server/server_SDC.js
+++ server/server_SDC.js
@@ -1,7 +1,6 @@
 require('dotenv').config({ path: '../.env' });
 
 const PORT = process.env.PORT || 6012;
-// const HOST = process.env.HOST || 'localhost';
 const HOST = 'localhost';
 
 const express = require('express');
@@ -19,26 +18,11 @@ const cors = require('cors');
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(cors());
 
-// const axios = require('axios');
 const createProxyMiddleware = require('http-proxy-middleware');
 
 app.use('/course', (req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
-
-// app.all('/course/item', async (req, res) => {
-//   await axios({
-//     method: req.method,
-//     url: process.env.COURSECONTENT_URL + req.originalUrl,
-//     data: req.body,
-//   })
-//     .then((results) => {
-//       res.status(200).json(results.data);
-//     })
-//     .catch((err) => {
-//       res.status(400).send(err);
-//     });
-// });
 
 app.use('/course/item', createProxyMiddleware({
   target: process.env.COURSECONTENT_URL,
